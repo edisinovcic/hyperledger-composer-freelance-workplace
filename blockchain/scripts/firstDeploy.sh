42010020be62a3dc4dadab20836e8f71dfbe31db
user <edi.sinovcic@gmail.com>
2018-12-21 21:54:17 +0100

    Updating config

--- blockchain/scripts/firstDeploy.sh
+++ blockchain/scripts/firstDeploy.sh
@@ -1,8 +1,9 @@
 #!bin/bash
-# First start fabric if fabric is not started in home repository
+# Only run first time or if something breaks
+cd ../fabric-dev-servers && ./startFabric.sh && ./createPeerAdminCard.sh && cd ../blockchain
 npm install
 npm run archive:create
 npm run local:install
-npm run local:start
 composer card import --file admin.card --card admin
 composer network ping -c admin
+npm run local:start
